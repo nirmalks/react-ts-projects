c75b5b4306c19cf6d5658920ec437faa90e54cd9
nirmalks <kagosekar@gmail.com>
2024-11-25 15:22:37 +0530

    add key for list and css

--- src/birthay-buddy/bdays.ts
+++ src/birthay-buddy/bdays.ts
@@ -4,34 +4,34 @@ export default [
     name: 'Scahin',
     age: 25,
     image:
-      '../assets/nirmal-cropped.png',
+      './src/assets/nirmal-cropped.png',
   },
   {
     id: 2,
     name: 'Shoni',
     age: 35,
     image:
-      '../assets/nirmal-cropped.png',
+      './src/assets/nirmal-cropped.png',
   },
   {
     id: 3,
     name: 'Virat',
     age: 45,
     image:
-      '../assets/nirmal-cropped.png',
+      './src/assets/nirmal-cropped.png',
   },
   {
     id: 4,
     name: 'Sehwag',
     age: 45,
     image:
-      '../assets/nirmal-cropped.png',
+      './src/assets/nirmal-cropped.png',
   },
   {
     id: 5,
     name: 'Dravid',
     age: 50,
     image:
-      '../assets/nirmal-cropped.png',
+      './src/assets/nirmal-cropped.png',
   },
 ];
