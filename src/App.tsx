1b8e575bcdab41d9bdb4a872cec4faac5e307036
nirmalks <kagosekar@gmail.com>
2024-11-26 13:01:01 +0530

    Add css for vertical center and missed tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ import bdays from './birthay-buddy/bdays';
 function App() {
   return (
     <>
-      <div className="container mx-auto px-4">
-        <BirthdayBuddy birthdays={bdays}></BirthdayBuddy>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="container mx-auto px-4">
+          <BirthdayBuddy birthdays={bdays}></BirthdayBuddy>
+        </div>
       </div>
     </>
   );
